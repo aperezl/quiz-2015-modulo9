803fecc6b6f10e4aa72317096a6ddf666e460f5e
Antonio Manuel Pérez López <aperezl@gmail.com>
2015-06-05 17:59:00 +0200

    Despliege en Heroku, P2P Módulo 7

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -16,8 +16,13 @@ exports.load = function(req, res, next, quizId) {
 
 // GET /quizes
 exports.index = function(req, res) {
-  models.Quiz.findAll().then(function(quizes) {
-    res.render('quizes/index.ejs', {quizes: quizes});
+  var where = {};
+  var search = req.query.search || '';
+  if(req.query.search) {
+    where = {where: ["pregunta like ?", '%' + search.replace(' ', '%') + '%']};
+  }
+  models.Quiz.findAll(where).then(function(quizes) {
+    res.render('quizes/index.ejs', {quizes: quizes, query: search});
   }).catch(function(error) { next(error);} );
 };
 
