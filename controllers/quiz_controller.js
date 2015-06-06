0b9f92a8ee016bd4cdf1ecfe3b96fdb8d2ece3b6
Antonio Manuel Pérez López <aperezl@gmail.com>
2015-06-06 11:10:14 +0200

    Modulo 8. Tema 4

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -92,3 +92,9 @@ exports.update = function(req, res) {
     }
   });
 };
+
+exports.destroy = function(req, res) {
+  req.quiz.destroy().then(function() {
+    res.redirect('/quizes');
+  }).catch(function(error) {next(error)});
+}
