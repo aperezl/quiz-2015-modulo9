5a617343d52453094fbd2dc9bb9f4fd09ea4a934
Antonio Manuel Pérez López <aperezl@gmail.com>
2015-08-05 00:02:09 +0200

    Módulo 9. Tema 3

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,3 +1,12 @@
+exports.loginRequired = function(req, res, next) {
+  if(req.session.user) {
+    next();
+  } else {
+    res.redirect('/login');
+  }
+};
+
+
 exports.new = function(req, res){
   var errors = req.session.errors || {};
   req.session.errors = {};
