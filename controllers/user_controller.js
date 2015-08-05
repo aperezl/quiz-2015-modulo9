d94d8c394ed32d6c4ed1c807edcfc7534a862ccf
Antonio Manuel Pérez López <aperezl@gmail.com>
2015-08-06 01:45:22 +0200

    Dev. Tema 21

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -1,8 +1,25 @@
 var models = require('../models/models.js');
 
-exports.autenticar = function(login, password, callback) {
+exports.load = function(req, res, next, userId) {
+  models.User.find({
+    where: {
+      id: Number(userId)
+    }
+  })
+  .then(function(user) {
+    if(user) {
+      req.user = user;
+      next();
+    } else {
+      next(new Error('No existe userId=' + userId));
+    }
+  })
+  .catch(function(error) {
+    next(error);
+  });
+};
 
-  console.log(models.User.find)
+exports.autenticar = function(login, password, callback) {
   models.User.find({
     where: {
       username: login
@@ -23,3 +40,74 @@ exports.autenticar = function(login, password, callback) {
     callback(error);
   });
 };
+
+exports.edit = function(req, res) {
+  res.render('user/edit', {user: req.user, errors: []});
+}
+
+exports.update = function(req, res) {
+  req.user.username = req.body.user.username;
+  req.user.password = req.body.user.password;
+
+  req.user
+  .validate()
+  .then(function(err) {
+    if(err) {
+      res.render('user/' + req.user.id, {user: req.user, errors: err.errors});
+    } else {
+      req.user
+      .save({ fields: ["username", "password"]})
+      .then(function() {
+        res.redirect('/');
+      });
+    }
+  })
+  .catch(function(error) {
+    next(error);
+  });
+
+
+}
+
+exports.new = function(req, res) {
+  var user = models.User.build({
+    username: "",
+    password: ""
+  });
+  res.render('user/new', {user: user, errors: []});
+}
+
+exports.create = function(req, res) {
+  var user = models.User.build(req.body.user);
+
+  user
+  .validate()
+  .then(function(err) {
+    if(err) {
+      res.render('user/new', {user: user, errors: []});
+    } else {
+      user
+      .save({fields: ["username", "password"]})
+      .then(function() {
+        req.session.user = {id:user.id, username: user.username};
+        res.redirect('/');
+      })
+    }
+  })
+  .catch(function(error) {
+    next(error);
+  });
+
+}
+
+
+
+exports.destroy = function(req, res) {
+  req.user.destroy().then(function() {
+    delete req.session.user;
+    res.redirect('/');
+  })
+  .catch(function(error) {
+    next(error);
+  });
+}
